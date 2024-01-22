024e2c8920f5c5fb2ad9017623a52100073bc0a6
Alger <69410170+alger24@users.noreply.github.com>
2024-01-22 15:07:02 +0800

    project day 35 done

---
+++ pd35 - Image Carousel/script.js
@@ -0,0 +1,48 @@
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+const imageContainer = document.querySelector('.image-container')
+
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0
+
+let interval = setInterval(run, 1000)
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length -1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(run, 2000)
+}
+
+right.addEventListener('click', () => {
+    idx++
+
+    changeImage()
+    resetInterval()
+
+    console.log('testingRight')
+}) 
+
+left.addEventListener('click', () => {
+    idx--
+
+    changeImage()
+    resetInterval()
+
+    console.log('testingLeft')
+})
